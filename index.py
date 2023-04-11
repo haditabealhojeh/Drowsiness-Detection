95d872f291cbf74cca9e80ec3ae3e44318f3d967
unknown <Administrator@actc.com>
2023-04-11 04:05:29 -0700

    Committer: unknown <Administrator@actc.com>

--- index.py
+++ index.py
@@ -16,10 +16,10 @@ def d_dtcn():
 
 	
 		
-	root.title("DROWSINESS DETECTION")
-	Label(root, text="DROWSINESS DETECTION",font=("times new roman",20),fg="black",bg="aqua",height=2).grid(row=2,rowspan=2,columnspan=5,sticky=N+E+W+S,padx=5,pady=10)
-	Button(root,text="Run using web cam",font=("times new roman",20),bg="#0D47A1",fg='white',command=function1).grid(row=5,columnspan=5,sticky=W+E+N+S,padx=5,pady=5)
-	Button(root,text="Run using phone cam",font=("times new roman",20),bg="#0D47A1",fg='white',command=function2).grid(row=7,columnspan=5,sticky=W+E+N+S,padx=5,pady=5)
-	Button(root,text="Exit",font=("times new roman",20),bg="#0D47A1",fg='white',command=root.destroy).grid(row=9,columnspan=5,sticky=W+E+N+S,padx=5,pady=5)
+	root.title("سیستم اعلام هشدار")
+	Label(root, text="سیستم اعلام هشدار خستگی رانندگان",font=("times new roman",20),fg="black",bg="aqua",height=2).grid(row=2,rowspan=2,columnspan=5,sticky=N+E+W+S,padx=5,pady=10)
+	Button(root,text="وب‌کم",font=("times new roman",20),bg="#0D47A1",fg='white',command=function1).grid(row=5,columnspan=5,sticky=W+E+N+S,padx=5,pady=5)
+	Button(root,text="موبایل",font=("times new roman",20),bg="#0D47A1",fg='white',command=function2).grid(row=7,columnspan=5,sticky=W+E+N+S,padx=5,pady=5)
+	Button(root,text="خروج",font=("times new roman",20),bg="#0D47A1",fg='white',command=root.destroy).grid(row=9,columnspan=5,sticky=W+E+N+S,padx=5,pady=5)
 
 	root.mainloop()
